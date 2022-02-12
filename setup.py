37a1bb002dcb7f29d6e54c870deb136ff7b6f966
aadmberrada <abdoulazizberrada@gmail.com>
2022-02-12 04:04:46 +0100

    new

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'abdoulaziz',         # How you named your package folder (MyLib)
   packages = ['abdoulaziz'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Abdoul Aziz Berrada',                   # Type in your name
