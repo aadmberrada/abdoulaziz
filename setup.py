77c2c4bf97e3296531b0595274bb8758a24c4979
aadmberrada <abdoulazizberrada@gmail.com>
2022-02-12 04:07:13 +0100

    new

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Abdoul Aziz Berrada',                   # Type in your name
   author_email = 'abdoulazizberrada@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/aadmberrada/abdoulaziz',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/aadmberrada/abdoulaziz/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/aadmberrada/abdoulaziz/archive/v_002.tar.gz',    # I explain this later on
   package_dir={'': '/Users/abdoul_aziz_berrada/Documents/Data Science'},
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
